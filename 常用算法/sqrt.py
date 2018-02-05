ca4cb37475ca4aa2e0b10314b78cd4c96ea3fb11
yihuf <925343614@qq.com>
2018-02-05 22:07:16 +0800

    解平方根方法
    分别使用二分法和牛顿法实现解平方根，其中牛顿法效率更高

---
+++ 常用算法/sqrt.py
@@ -0,0 +1,45 @@
+pricision = 0.00001    #设置精度值
+
+'''通过二分法实现开根号'''
+def sqrt1(number):
+    max = number
+    min = 0
+    mid = (max + min) / 2
+    count = 0
+    while True:
+        count += 1
+        if (number - mid * mid).__abs__() < pricision:
+            break
+        mid = (min + max) / 2
+        if (mid * mid) > number:
+            max = mid
+        elif mid * mid < number:
+            min = mid
+        else:
+            break
+
+    print("value:{},count:{}".format(mid, count))
+
+
+'''通过牛顿法实现开根号'''
+def sqrt2(number):
+    x = 1  #设置一个随机值
+    count = 0
+    while True:
+        count += 1
+        if (number - x * x).__abs__() < pricision:
+            break
+        x = (x + number / x) / 2
+
+    print("value:{},count:{}".format(x, count))
+
+sqrt1(2.25)
+sqrt1(9)
+sqrt1(25)
+sqrt1(100)
+sqrt2(2.25)
+sqrt2(9)
+sqrt2(25)
+sqrt2(100)
+
+'''使用牛顿法可以显著提高求根效率'''
